fdc98102b84dafdfecc01379a7ecb01ee014a3f9
Ilya Barygin <barygin@gmail.com>
2013-09-09 22:25:23 +0400

    Change layout a bit

--- application/views/admin/add_group.php
+++ application/views/admin/add_group.php
@@ -5,6 +5,7 @@ echo form_open('admin/add_group');
 <h3>Добавить отряд</h3>
 <p>Год:
 <?php echo form_input(array('name' => 'year', 'size' => 4));?>
+
 Название:
 <?php echo form_input(array('name' => 'name', 'size' => 10));?>
 <?php echo form_submit('submit', 'Добавить');?>
--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 <p>
 <?php if (isset($admin) && $admin == true): ?>
 <?php echo anchor('admin', 'Администрирование'); ?>
-<br/>
+
 <?php endif; ?>
 <?php echo anchor('lager', 'На главную'); ?>
 </p>
