935ec1ac9f455bade6b26874780f0595f60c80da
Ilya Barygin <barygin@gmail.com>
2013-09-15 01:01:32 +0400

    Valid XHTML

--- application/views/group.php
+++ application/views/group.php
@@ -5,8 +5,8 @@
 <div class="p1">
 <a href="<?php echo site_url('lager/person/'.$person->person_id.'/'.$group->group_id); ?>">
 <img class="photo" src="<?php echo $images[$person->person_id]; ?>" alt="<?php echo $person->name; ?>" />
-<div class="pn"><?php echo $person->name; ?></div>
 </a>
+<div class="pn"><?php echo anchor('lager/person/'.$person->person_id.'/'.$group->group_id, $person->name); ?></div>
 </div>
 <?php endforeach;?>
 </div>
