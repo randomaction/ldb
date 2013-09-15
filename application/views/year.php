d00f5cf8b5230a5f75903ca0ee07b70ae367c6ec
Ilya Barygin <barygin@gmail.com>
2013-09-15 11:05:48 +0400

    Column view for year

--- application/views/year.php
+++ application/views/year.php
@@ -1,10 +1,12 @@
 <?php $this->load->helper('url');?>
 <h2><?php echo $year; ?></h2>
 <?php foreach ($group_data as $group):?>
+<div class="y">
 <h3><?php echo anchor("lager/group/".$group->group_id, $group->name); ?></h3>
 <ul>
 <?php foreach ($groups[$group->name] as $person):?>
 <li><?php echo anchor("lager/person/".$person->person_id.'/'.$group->group_id, $person->name); ?></li>
 <?php endforeach;?>
 </ul>
+</div>
 <?php endforeach;?>
