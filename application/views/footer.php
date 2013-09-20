ccb8db951b29adaa587107fd2f68d36f7c51d275
Ilya Barygin <barygin@gmail.com>
2013-09-20 21:15:25 +0400

    Kinda improvement of header

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,2 @@
-<?php $this->load->helper('url'); ?>
-<p>
-<?php echo anchor('lager', 'На главную'); ?>
-</p>
 </body>
 </html>
--- application/views/header.php
+++ application/views/header.php
@@ -1,17 +1,24 @@
 <?php
 $this->load->helper('url');
 $username = $this->session->userdata('username');
+$site_title = 'Летняя Физическая Школа';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="<?php echo base_url('media/main.css'); ?>" type="text/css" />
-<title>Летняя физическая школа</title>
+<title><?php echo $site_title; ?></title>
 </head>
 <body>
+<div id="header">
+<div id="headpic"> </div>
+<div id="headcont"><h2>
+<?php echo anchor('', $site_title, array('id' => 'headlink')); ?>
+</h2></div>
+</div>
 <?php if ( $username != null) : ?>
-<p>[<?php echo $username; ?>]
+<p class="admininfo">[<?php echo $username; ?>]
 
 <?php echo anchor('admin', 'Администрирование'); ?>
 
